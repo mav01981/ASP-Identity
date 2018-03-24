a96348e5d631c1a5fcbf42e1721a5d7c90a2dd3f
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-03-24 18:49:15 +0000

    Initial

---
+++ Models/IdentityModels.cs
@@ -0,0 +1,125 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace WebApplication5.Models
+{
+    public class CustomUser : IUser<string>
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+
+        internal Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager userManager)
+        {
+            return Task.FromResult(new ClaimsIdentity());
+        }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<CustomUser> Users { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+    public class CustomUserStore : IUserStore<CustomUser>, IUserPasswordStore<CustomUser>, IUserEmailStore<CustomUser>
+    {
+        private readonly ApplicationDbContext context;
+
+
+        public CustomUserStore(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task CreateAsync(CustomUser user)
+        {
+            user.Id = Guid.NewGuid().ToString();
+
+            context.Users.Add(user);
+            return context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(CustomUser user)
+        {
+            context.Users.Remove(user);
+            return context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task<CustomUser> FindByEmailAsync(string email)
+        {
+            return Task.FromResult(new CustomUser());
+        }
+
+        public Task<CustomUser> FindByIdAsync(string userId)
+        {
+            return context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public Task<CustomUser> FindByNameAsync(string userName)
+        {
+            return context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+        }
+
+        public Task<string> GetEmailAsync(CustomUser user)
+        {
+            return Task.FromResult(user.Email);
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(CustomUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(CustomUser user)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(CustomUser user)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetEmailAsync(CustomUser user, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetEmailConfirmedAsync(CustomUser user, bool confirmed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(CustomUser user, string passwordHash)
+        {
+            user.PasswordHash = passwordHash;
+
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(CustomUser user)
+        {
+            context.Users.Attach(user);
+            return context.SaveChangesAsync();
+        }
+    }
+
+}
